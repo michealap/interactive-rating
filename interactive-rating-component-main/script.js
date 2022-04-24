e65b7076a566a5562da19558086125ababa6b8ff
Micheala Palmer <michealapalmer@gmail.com>
2022-04-24 03:19:23 -0400

    add scripts for rating selection

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,31 @@
+const ratingBtns = document.querySelectorAll(".rating-btn");
+const ratingCard = document.querySelector(".rating-card");
+const thankYouCard = document.querySelector(".thank-you-card");
+const ratingScore = document.getElementById("selectedRating");
+const submitBtn = document.querySelector('.submit-btn');
+
+let selectedRating = null;
+
+submitBtn.addEventListener('click', handleSubmit);
+ratingBtns.forEach((btn) => {
+  btn.addEventListener('click', handleRating);
+});
+
+function handleSubmit() {
+  if (!selectedRating) {
+    return alert("Please select a rating.");
+  }
+  ratingCard.classList.add('hidden');
+  ratingScore.textContent = selectedRating;
+  thankYouCard.classList.remove('hidden');
+}
+
+function handleRating({ target }) {
+  ratingBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+
+  target.classList.add('active');
+
+  selectedRating = event.target.textContent;
+}
